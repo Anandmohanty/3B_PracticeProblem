f4eba2d0cbea4fa4c8f7cdd646a4048ed72bc527
Anandmohanty <mohantyanand3@gmail.com>
2023-07-03 20:32:42 +0530

    [Anand] - Type Of Constructor

---
+++ TypeOfConstructor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3B_PracticeProblem
+{
+    public class TypeOfConstructor
+    {
+        //Default Constructor
+        public TypeOfConstructor()
+        {
+            Console.WriteLine("Default Constructor Active");
+        }
+
+        //Parameterized Constructor
+        string userName;
+        int userAge;
+        public TypeOfConstructor(string name, int age)
+        {
+            userName = name;
+            userAge = age;
+            Console.WriteLine("Parameterized Constructor Active and Parameter given is \n User Name" + name + " User Age " + age);
+        }
+
+        //Copy Constructor 
+        public TypeOfConstructor(TypeOfConstructor typeOfConstructor)
+        {
+            Console.WriteLine("Copy Constructor is Active");
+        }
+
+        //Static Constructor
+        static TypeOfConstructor()
+        {
+            Console.WriteLine("Static Constructor Active");
+        }
+        public static void SampleMessage()
+        {
+            Console.WriteLine("This is Sample Message for Static class activation");
+        }
+
+        //
+    }
+}
+
