530909d6e93fd6801d0102def22ae6292cf8a83c
Anandmohanty <mohantyanand3@gmail.com>
2023-07-03 20:08:05 +0530

    [Anand] - Welcome to Practice problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3B_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! To Practice Problem");
+        }
+    }
+}
